2f2fddd146cbb38ecfa94f80818259b2e3a54ed0
Alex Gorchinskiy <gorchinski73@gmail.com>
2023-07-21 06:30:40 +0300

    Сделано домашнее задание

---
+++ task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetDegree(int numberA, int numberB)
+ {
+     int result = 1;
+     for (int i = 1; i <= numberB; i++)
+     {
+         result = result * numberA;
+     }
+     return result;
+ }
+
+ int GetInput(string text)
+ {
+     Console.Write(text);
+     return Convert.ToInt32(Console.ReadLine());    
+ }
+
+ int numberA = GetInput("Введите число А: ");
+ int numberB = GetInput("Введите число B: ");
+ int result = GetDegree(numberA, numberB);
+ Console.WriteLine($"Натуральная степень числа {numberA} в степени {numberB} равно {result}");
---
+++ task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int DigitSum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        sum = sum + digit;
+        number /= 10;
+    }
+    return sum;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number1 = GetInput("Введите последовательность чисел: ");
+int number = Math.Abs(number1);
+Console.WriteLine($"Сумма всех чисел в последовательности {number} = {DigitSum(number)}");
